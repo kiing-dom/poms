e6a7e2a33b6db727187e134077df9266bf0af186
Dominion Gbadamosi <worshipgbadamosi@live.com>
2025-08-29 00:17:26 +0100

    structural change

--- cmd/pomodoro/main.go
+++ cmd/pomodoro/main.go
@@ -9,28 +9,22 @@ func main() {
 	breakDuration := 1 * time.Second
 
 	fmt.Println("Starting", workDuration, "second", "pomodoro...")
-	pomTimer := time.NewTimer(workDuration)
-
 	workSeconds := int(workDuration.Seconds())
 	for i := workSeconds; i > 0; i-- {
 		fmt.Println(i, "seconds left...")
 		time.Sleep(1 * time.Second)
 	}
 
-	<- pomTimer.C
-
 	fmt.Println("Pomodoro ended! Good Job.")
 	time.Sleep(2 * time.Second)
 
 	fmt.Println("Starting ", breakDuration, "second ", "break.")
-	breakTimer := time.NewTimer(breakDuration)
 
 	breakSeconds := int(breakDuration.Seconds())
 	for i := breakSeconds; i > 0; i-- {
 		fmt.Println(i, "seconds left")
 	}
 
-	<- breakTimer.C
 	fmt.Println("Break over. Pomodoro session complete")
 	
 }
