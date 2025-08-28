263e448e5d1b8d7e58cf0bec8c29f715c7a7bc95
Dominion Gbadamosi <worshipgbadamosi@live.com>
2025-08-28 23:38:56 +0100

    Add: added countdown timer for pomodoros and breaks

--- main.go
+++ main.go
@@ -5,15 +5,30 @@ import (
 )
 
 func main() {
-	fmt.Println("Starting 2 second pomodoro...")
-	pomTimer := time.NewTimer(2 * time.Second)
+	workDuration := 2 * time.Second
+	breakDuration := 1 * time.Second
+
+	fmt.Println("Starting", workDuration, "second", "pomodoro...")
+	pomTimer := time.NewTimer(workDuration)
+
+	workSeconds := int(workDuration.Seconds())
+	for i := workSeconds; i > 0; i-- {
+		fmt.Println(i, "seconds left...")
+		time.Sleep(1 * time.Second)
+	}
 
 	<- pomTimer.C
 
 	fmt.Println("Pomodoro ended! Good Job.")
 	time.Sleep(2 * time.Second)
-	fmt.Println("Starting 1 second break.")
-	breakTimer := time.NewTimer(1 * time.Second)
+
+	fmt.Println("Starting ", breakDuration, "second ", "break.")
+	breakTimer := time.NewTimer(breakDuration)
+
+	breakSeconds := int(breakDuration.Seconds())
+	for i := breakSeconds; i > 0; i-- {
+		fmt.Println(i, "seconds left")
+	}
 
 	<- breakTimer.C
 	fmt.Println("Break over. Pomodoro session complete")
